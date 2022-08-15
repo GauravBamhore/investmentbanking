0144157f5b37a9968fbc65ad2c656916017c55fd
Gaurav <gauravbamhore22@gmail.com>
2022-08-15 09:53:30 +0530

    new commit from ib

--- src/test/java/KiteTest/TC_001.java
+++ src/test/java/KiteTest/TC_001.java
@@ -39,7 +39,7 @@ public class TC_001 extends Base{
   {
 	  if(name.equals("chrome"))
 	  {
-		  chromeBrowser();
+		  chromeBrowser();//chromebrowser
 	  }
 	  else if (name.equals("firefox"))
 	  {
